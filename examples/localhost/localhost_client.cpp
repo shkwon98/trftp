3729683a3a48a8e7ae3cfda9dae96fcc8535d178
shkwon98 <seonghyeon.kwon@avikus.ai>
2024-10-31 15:56:53 +0900

    Implement localhost server and client examples

--- examples/localhost/localhost_client.cpp
+++ examples/localhost/localhost_client.cpp
@@ -1,6 +1,44 @@
+#include <condition_variable>
+#include <filesystem>
+#include <iostream>
+#include <mutex>
+
 #include <trftp/client/client.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    return 0;
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const auto port = std::stoi(argv[1]);
+
+    if (port < 1024 || port > 65535)
+    {
+        std::cerr << "Invalid port: " << port << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    trftp::Client client(port);
+    std::cout << "Waiting for file transfer..." << std::endl;
+
+    std::condition_variable cv;
+    std::mutex m;
+
+    client.AttachFileHandler([&cv](const std::string &file_path, const std::uint32_t version) {
+        std::filesystem::path rx_path = "trftp_file";
+        std::filesystem::rename(file_path, rx_path);
+        std::cout << "File received: " << rx_path << " (version: " << version << ")" << std::endl;
+        cv.notify_one();
+    });
+
+    if (std::unique_lock lock(m); cv.wait_for(lock, std::chrono::seconds(30)) == std::cv_status::timeout)
+    {
+        std::cerr << "Timeout" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
--- examples/localhost/localhost_server.cpp
+++ examples/localhost/localhost_server.cpp
@@ -1,6 +1,56 @@
+#include <filesystem>
+#include <iostream>
+
 #include <trftp/server/server.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    return 0;
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <file_path> <client_port>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const auto file_path = std::filesystem::path(argv[1]);
+    const auto client_port = std::stoi(argv[2]);
+    const auto client_uri = "127.0.0.1:" + std::to_string(client_port);
+
+    if (!std::filesystem::exists(file_path))
+    {
+        std::cerr << "File does not exist: " << file_path << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (!std::filesystem::is_regular_file(file_path))
+    {
+        std::cerr << "Not a regular file: " << file_path << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (client_port < 1024 || client_port > 65535)
+    {
+        std::cerr << "Invalid port: " << client_port << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    trftp::Server server;
+
+    std::cout << "Starting file transfer to " << client_uri << "..." << std::endl;
+    const auto result = server.StartFileTransfer(client_uri, file_path, 0);
+
+    if (result == trftp::FtpStatus::NTF)
+    {
+        std::cerr << "Cannot find client: " << client_uri << std::endl;
+        return EXIT_FAILURE;
+    }
+    else if (result == trftp::FtpStatus::CXL)
+    {
+        std::cerr << "Transfer cancelled by error" << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        std::cout << "Successfully transferred file to " << client_uri << std::endl;
+    }
+
+    return EXIT_SUCCESS;
 }
